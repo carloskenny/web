132ca9e9c1dea627a077f9b63ec1ad54d6c75dd2
Carlos Kenny <carloskenny@live.com>
2023-05-18 17:50:15 -0300

    🎉 init: project web template

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-start justify-between p-24">
+        <h1>Projeto Base com Nextjs. TailwindCSS ESlint</h1>
+      
+    </main>
+  )
+}
